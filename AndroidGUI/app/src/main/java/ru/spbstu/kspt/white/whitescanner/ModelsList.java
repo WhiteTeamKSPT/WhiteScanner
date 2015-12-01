7786ca49c83930a7807249e7f7079a3ab3e1c38d
Artyom Aleksyuk <artyom.h31@gmail.com>
2015-12-02 02:49:33 +0300

    Fetch models from server

--- AndroidGUI/app/src/main/java/ru/spbstu/kspt/white/whitescanner/ModelsList.java
+++ AndroidGUI/app/src/main/java/ru/spbstu/kspt/white/whitescanner/ModelsList.java
@@ -1,23 +1,39 @@
 package ru.spbstu.kspt.white.whitescanner;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Victor on 24.10.2015.
  */
 
 
-public class ModelsList extends AppCompatActivity {
+public class ModelsList extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     public final static String EXTRA_MESSAGE = "ru.spbstu.kspt.white.whitescanner.MODEL_NAME";
+    private static final String COMPONENT = "ModelsList";
 
     ListView listView ;
+    SwipeRefreshLayout mSwipeRefreshLayout;
+    ArrayAdapter<String> adapter;
+
+    ArrayList<String> listViewItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +48,12 @@ public class ModelsList extends AppCompatActivity {
                 "coke_can",
                 "skateboard"
         };
+        Collections.addAll(listViewItems, values);
+
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+        // делаем повеселее
+//        mSwipeRefreshLayout.setColorSchemeResources(R.color.blue, R.color.green, R.color.yellow, R.color.red);
 
         // Define a new Adapter
         // First parameter - Context
@@ -39,8 +61,8 @@ public class ModelsList extends AppCompatActivity {
         // Third parameter - ID of the TextView to which the data is written
         // Forth - the Array of data
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, listViewItems);
 
         // Assign adapter to ListView
         listView.setAdapter(adapter);
@@ -58,7 +80,13 @@ public class ModelsList extends AppCompatActivity {
                 // ListView Clicked item value
                 String itemValue = (String) listView.getItemAtPosition(position);
 
-                // Show model
+//                try {
+//                    int number = Integer.parseInt(itemValue);
+//
+//                } catch (NumberFormatException e) {
+//                    // Show model
+//                    showModel(itemValue);
+//                }
                 showModel(itemValue);
             }
 
@@ -73,4 +101,47 @@ public class ModelsList extends AppCompatActivity {
         startActivity(intent);
     }
 
+    @Override
+    public void onRefresh() {
+        mSwipeRefreshLayout.setRefreshing(true);
+        new GetPhotos().execute();
+    }
+
+    private class GetPhotos extends AsyncTask<Void, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(Void... params) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                Iterator<Integer> iterator = Requests.pendingRequests.listIterator();
+                while (iterator.hasNext()) {
+                    Integer set = iterator.next();
+                    Log.d(COMPONENT, "Fetching set " + set.toString());
+                    String url = Network.makeResultURL("user", set);
+                    HttpResponse resp = Network.doGET(url);
+                    if (resp.code != 500) {
+                        Log.d(COMPONENT, "Set is fetched: " + set.toString());
+                        Requests.models.put(set, resp.data);
+                        iterator.remove();
+                    } else {
+                        Log.d(COMPONENT, "Set not ready: " + set.toString());
+                    }
+                }
+            } catch (IOException e) {
+                Log.e(COMPONENT, e.toString());
+                return false;
+            }
+            return true;
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(Boolean result) {
+            listViewItems.clear();
+            for (Integer model: Requests.models.keySet()) {
+                listViewItems.add(model.toString());
+            }
+            adapter.notifyDataSetChanged();
+            mSwipeRefreshLayout.setRefreshing(false);
+        }
+    }
 }
---
+++ AndroidGUI/app/src/main/java/ru/spbstu/kspt/white/whitescanner/Network.java
@@ -0,0 +1,120 @@
+package ru.spbstu.kspt.white.whitescanner;
+
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by artyom on 02.12.15.
+ */
+public class Network {
+    private static final String COMPONENT = "Network";
+    static final String BASE_URL = "http://whiteteam.cloudapp.net:8080";
+    static final String UPLOAD_PATH = "/client/upload";
+    static final String FINISH_PATH = "/client/finished";
+    static final String RESULT_PATH = "/client/result";
+
+    public static String makeUploadURL(String user, int set, int number) {
+        return BASE_URL + UPLOAD_PATH + "/" + user + "/" + set + "/" + number + "/";
+    }
+
+    public static String makeFinishURL(String user, int set) {
+        return BASE_URL + FINISH_PATH + "/" + user + "/" + set + "/";
+    }
+
+    public static String makeResultURL(String user, int set) {
+        return BASE_URL + RESULT_PATH + "/" + user + "/" + set + "/";
+    }
+
+    // Given a URL, establishes an HttpUrlConnection and retrieves
+    // the web page content as a InputStream, which it returns as
+    // a string.
+    public static void doPOST(String uploadURL, @Nullable byte[] payload) throws IOException {
+        OutputStream os = null;
+        Log.d(COMPONENT, "Do POST to " + uploadURL);
+
+        try {
+            URL url = new URL(uploadURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            if (payload != null) {
+                Log.d(COMPONENT, "Payload size is  " + payload.length);
+                conn.setFixedLengthStreamingMode(payload.length);
+            }
+            // Starts the query
+            conn.connect();
+            os = conn.getOutputStream();
+            if (payload != null) {
+                os.write(payload);
+            }
+            os.flush();
+            os.close();
+
+            int response = conn.getResponseCode();
+            Log.d(COMPONENT, "The response is: " + response);
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (os != null) {
+                os.close();
+            }
+        }
+    }
+
+    public static HttpResponse doGET(String uploadURL) throws IOException {
+        InputStream is = null;
+        Log.d(COMPONENT, "Do GET to " + uploadURL);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        HttpResponse response = new HttpResponse();
+
+        try {
+            URL url = new URL(uploadURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestProperty("User-Agent", "Mozilla/5.0 ( compatible ) ");
+            conn.setRequestProperty("Accept", "*/*");
+
+            // Starts the query
+            conn.connect();
+
+            response.code = conn.getResponseCode();
+            Log.d(COMPONENT, "The response is: " + response.code);
+            // TODO
+            if (response.code == 400) {
+                is = conn.getInputStream();
+                byte[] buffer = new byte[4096];
+                int n;
+
+                while ((n = is.read(buffer)) > 0) {
+                    baos.write(buffer, 0, n);
+                }
+            }
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+        response.data = baos.toByteArray();
+        return response;
+    }
+}
+
+class HttpResponse {
+    byte[] data;
+    int code;
+}
